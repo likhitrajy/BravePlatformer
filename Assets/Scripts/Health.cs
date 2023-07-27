d2df249753097ae34c5543d2be58cdda5ad0bb7c
likhitrajy <likhitrajy@gmail.com>
2023-07-27 13:41:49 -0400

    Added Health System, Player's punch system (raw).

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+   public int maxHealth = 100;
+   int currentHealth;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(int damage){
+        currentHealth -= damage;
+        if(currentHealth <= 0){
+            Die();
+        }
+    }
+
+    void Die(){
+        
+        Destroy(gameObject);
+    }
+}
